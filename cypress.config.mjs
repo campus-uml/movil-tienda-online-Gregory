3dced35ad4efabc90efb7570bee2edf1a40f61c7
Gregory Enriquez <est.ggomeze0201@uml.edu.ni>
2025-02-15 20:12:43 -0600

    Integracion codecov fallida

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -4,14 +4,12 @@ export default defineConfig({
   projectId: "358q26",
   e2e: {
     baseUrl: "http://localhost:5173",
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Ruta para pruebas E2E
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 
   component: {
-    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}", // Ruta para pruebas de componentes
     devServer: {
       framework: "react",
       bundler: "vite",
